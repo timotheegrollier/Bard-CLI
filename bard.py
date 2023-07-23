8af5fc0162a16b01ee22ce978a9b23835bcf102b
Timothée Grollier <timotheegrollier@gmail.com>
2023-07-23 19:41:59 +0200

    first commit

---
+++ bard.py
@@ -0,0 +1,34 @@
+from bardapi import Bard
+from colorama import init, Fore, Style
+
+def get_user_input():
+    """Get user input in a loop until 'exit' is entered."""
+    while True:
+        prompt = input("You: ")
+        if prompt.lower() == 'exit':
+            print(f"{Fore.YELLOW}Exiting Bard CLI. Goodbye!\n")
+            break
+        yield prompt
+
+def main():
+    # Initialize colorama to work with Windows terminals as well
+    init(autoreset=True)
+
+    welcome_message = (
+        f"{Fore.GREEN}Welcome to Bard CLI! Type 'exit' to end the session.\n"
+        f"{Fore.YELLOW}Bard will assist you in answering your questions.\n"
+        f"{Fore.YELLOW}Type your question or input, and Bard will provide a response.\n"
+        f"{Fore.YELLOW}To exit the session, simply type 'exit'.\n"
+        f"{Style.RESET_ALL}"
+    )
+    print(welcome_message)
+
+    bard = Bard(token_from_browser=True)
+
+    for prompt in get_user_input():
+        res = bard.get_answer(prompt)
+        print()
+        print(f"{Fore.CYAN}Bard:", f"{Style.RESET_ALL}{res['content']}\n")
+
+if __name__ == "__main__":
+    main()
